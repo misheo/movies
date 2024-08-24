3632ab6bbb2b33de4ff3bb59be3356265dab5365
Said Nassar <saidnassar18@gmail.com>
2024-08-24 18:38:04 +0300

    Update Splash Screen and Android/iOS Launch Configuration
    - Added new background images for Android and iOS launch screens.
    - Updated `launch_background.xml` to use the new background images for both Android and Android 12+ configurations.
    - Added new styles and configurations for Android 12+ and iOS to handle splash screens more effectively.
    - Introduced a new `flutter_native_splash.yaml` configuration file to manage splash screen settings across platforms.
    - Updated iOS `LaunchScreen.storyboard` and `Info.plist` to include new background images and configurations.
    - Added a new Dart file `navigator_screen.dart` for potential future navigation features.
    - Updated web `index.html` to include new splash screen styles and script for web platforms.
    - Added a new splash background image for web platforms.
    
    This commit consolidates and enhances the splash screen experience across Android, iOS, and Web platforms, ensuring a consistent and customizable launch experience.

---
+++ lib/features/navigator/navigator_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ButtonNavigator extends StatelessWidget {
+  const ButtonNavigator({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ButtonNavigator'),
+      ),
+      body: Center(
+        child: Text('ButtonNavigator'),
+      ),
+    );
+  }
+}
