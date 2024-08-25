d1ba37227ed63c31abb866176cc264f039691fc0
Said Nassar <saidnassar18@gmail.com>
2024-08-25 03:56:41 +0300

    Refactor PopularMovieRepoImpl to use ApiService for fetching popular movies
    - Implemented the `getPopularMovies` method in `PopularMovieRepoImpl` to utilize `ApiService` for fetching popular movies data.
    - Added dependency on `ApiService` and `ApiConstants` for handling network requests and constants respectively.
    - Integrated error handling using `DioException` to manage failures in API requests.
    - Removed the placeholder `UnimplementedError` and replaced it with a functional implementation.

--- lib/features/home/data/repo/popular_movie_impl.dart
+++ lib/features/home/data/repo/popular_movie_impl.dart
@@ -1,14 +1,26 @@
+import 'package:dio/dio.dart';
+import 'package:movies/core/networking/api_constants.dart';
 import 'package:movies/core/networking/api_result.dart';
 
 import 'package:movies/features/home/data/movie_response_body.dart';
 
+import '../../../../core/networking/api_service.dart';
 import 'popular_movie_repo.dart';
 
 class PopularMovieRepoImpl implements PopularMovieRepo {
+  final ApiService _apiService;
+
+  PopularMovieRepoImpl(this._apiService);
   @override
-  Future<ApiResult<MovieResponseBody>> getPopularMovies() {
-    // TODO: implement getPopularMovies
-    throw UnimplementedError();
+  Future<ApiResult<MovieResponseBody>> getPopularMovies() async {
+    try {
+      String authorization = ApiConstants.authorization;
+      final response = await _apiService.getPopularMovies(authorization);
+      return ApiResult.success(response) ;
+      
+    } on DioException catch (e) {
+      return ApiResult.failure(e.response?.data);
+    }
   }
   
 }
