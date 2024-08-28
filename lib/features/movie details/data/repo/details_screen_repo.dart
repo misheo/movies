de735d4f2b086cc3d546b48d13d6a8bb99f7a39a
Said Nassar <saidnassar18@gmail.com>
2024-08-28 21:22:33 +0300

    Add Movie Details Repository and Dependency Injection Setup
    This commit introduces the implementation of the movie details repository, including methods to fetch movie details and similar movies by ID. It also sets up the dependency injection for this repository within the application's core dependency injection module. This enhancement allows for more modular and scalable data handling within the movie details feature, ensuring that the application can now retrieve and display detailed movie information and similar movie recommendations.

---
+++ lib/features/movie details/data/repo/details_screen_repo.dart
@@ -0,0 +1,10 @@
+import 'package:movies/core/networking/api_result.dart';
+import 'package:movies/features/home/data/movie_response_body.dart';
+
+import '../../../../core/models/movie.dart';
+
+abstract class DetailsScreenRepo {
+  Future<ApiResult<Movie>> getMovieDetails(int movieId);
+  Future<ApiResult<MovieResponseBody>> getSimilar(int movieId);
+  
+}
